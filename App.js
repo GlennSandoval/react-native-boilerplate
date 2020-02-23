5174e79779bf1dc7aea43c76d4b5c969c90349a5
Glenn Sandoval <glenn.sandoval@gmail.com>
2020-02-23 16:34:29 +1300

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Home from './screens/Home';
+import About from './screens/About';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="About" component={About} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
