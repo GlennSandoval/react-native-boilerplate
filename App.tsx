6a3737001e642abc9cc2c26169f6b05f4f6f3d69
Glenn Sandoval <glenn.sandoval@gmail.com>
2020-03-08 13:03:25 +1300

    Add custom header

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
-import {TransitionSpec} from '@react-navigation/stack/lib/typescript/src/types';
+import {
+  StackHeaderProps,
+  TransitionSpec,
+} from '@react-navigation/stack/lib/typescript/src/types';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Easing} from 'react-native';
+
+import {Header, Body, Left, Button, Icon, Right, Title} from 'native-base';
 
 import Home from './screens/Home';
 import About from './screens/About';
@@ -17,15 +22,53 @@ const config: TransitionSpec = {
   },
 };
 
+const AppHeader = (props: StackHeaderProps) => {
+  const {scene, previous, navigation} = props;
+  const {options} = scene.descriptor;
+  const title =
+    options.headerTitle !== undefined
+      ? options.headerTitle
+      : options.title !== undefined
+      ? options.title
+      : scene.route.name;
+
+  return (
+    <Header>
+      <Left>
+        {!!previous && (
+          <Button transparent onPress={navigation.goBack}>
+            <Icon name="arrow-back" />
+          </Button>
+        )}
+      </Left>
+      <Body>
+        <Title>{title}</Title>
+      </Body>
+      <Right>
+        <Button transparent>
+          <Icon name="menu" />
+        </Button>
+      </Right>
+    </Header>
+  );
+};
+
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Navigator headerMode="screen">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{header: AppHeader}}
+        />
         <Stack.Screen
           name="About"
           component={About}
-          options={{transitionSpec: {open: config, close: config}}}
+          options={{
+            transitionSpec: {open: config, close: config},
+            header: AppHeader,
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
